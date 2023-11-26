edf6fc72b91a4c17449b15c5ef26c3426912a37e
Andrzej <andrzej1250@hotmail.com>
2023-11-26 13:50:38 +0100

    Scanner

--- src/main/java/org/weatherapp/Main.java
+++ src/main/java/org/weatherapp/Main.java
@@ -6,6 +6,7 @@ import org.weatherapp.webclient.WeatherApiClient;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -13,17 +14,27 @@ public class Main {
         WeatherToForecastMapper weatherToForecastMapper = new WeatherToForecastMapper();
         final HttpResponse<String> response = weatherApiClient.getWeather("Gdynia");
 
-        System.out.println("To jest JSON: ");
-        System.out.println(response.body());
+//        System.out.println("To jest JSON: ");
+//        System.out.println(response.body());
 
 
         Forecast forecast = weatherToForecastMapper.mapToForecast(response.body());
         System.out.println("To jest nasz obiekt:");
         System.out.println(forecast.toString());
 
-        final HttpResponse<String> historicalForecast = weatherApiClient.getHistoricalWeather("Gdynia", "2015-01-21");
-        System.out.println("To jest nasz historical forecast:");
+//        final HttpResponse<String> historicalForecast = weatherApiClient.getHistoricalWeather("Gdynia", "2015-01-21");
+//        System.out.println("To jest nasz historical forecast:");
+//
+//        System.out.println(historicalForecast.body());
 
-        System.out.println(historicalForecast.body());
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj miasto: ");
+        String cityScanner = scanner.nextLine();
+
+        final HttpResponse<String> responseScanner = weatherApiClient.getWeather(String.valueOf(cityScanner));
+        Forecast forecastScanner = weatherToForecastMapper.mapToForecast(responseScanner.body());
+
+        System.out.println(forecastScanner.toString());
     }
 }
