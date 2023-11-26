276839f760dd8c4b1ba73ab6d5efc77dfb3934c0
Andrzej <andrzej1250@hotmail.com>
2023-11-26 16:03:36 +0100

    FileWriter2

--- src/main/java/org/weatherapp/webclient/FileWriterExample.java
+++ src/main/java/org/weatherapp/webclient/FileWriterExample.java
@@ -9,17 +9,18 @@ public class FileWriterExample {
 
     public void writeToFile(Forecast forecastScanner) {
         // Ścieżka do pliku, do którego chcesz zapisać dane
-        String filePath = "src/main/java/org/weatherapp/webclient/CurrentWeather.txt";
+        String filePath = "src/main/resources/CurrentWeather.txt";
 
         // Dane do zapisania
         String dataToWrite = forecastScanner.toString();
 
         try {
             // Tworzenie obiektu BufferedWriter dla efektywnego zapisu
-            BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(filePath));
+            BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(filePath, true ));
 
             // Zapis danych do pliku
             writer.write(dataToWrite);
+            writer.newLine();
 
             // Zamknięcie writer'a, aby zwolnić zasoby
             writer.close();
